4b51ddd35b73da69d3a6b938ca6468ad3cb54e41
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-05-16 22:29:30 +0300

    Updating code according to udacity Code Review.

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ import rps
 import players
 import random
 
+
 if __name__ == '__main__':
-    if __name__ == '__main__':
-        players = [
-            players.AllRockPlayer(),
-            players.RandomPlayer(),
-            players.ReflectPlayer(),
-            players.CyclePlayer()
-        ]
-        p1 = players.HumanPlayer()
-        p2 = random.choice(players)
-        game = rps.Game(p1, p2)
-        game.play_game()
+    game_players = [
+        players.AllRockPlayer(),
+        players.RandomPlayer(),
+        players.ReflectPlayer(),
+        players.CyclePlayer()
+    ]
+    p1 = players.HumanPlayer()
+    p2 = random.choice(game_players)
+    game = rps.Game(p1, p2)
+    game.play_game()
