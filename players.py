a54e5158b6acc0693665461f02034164101d641b
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-05-16 21:38:50 +0300

    Updating code according to udacity Code Review

--- players.py
+++ players.py
@@ -1,6 +1,7 @@
 """This module contains different type of players for rps.py"""
 import random
 
+
 class Player:
     """ Base class for players, providing common attributes and methods."""
     moves = ['rock', 'paper', 'scissors']
@@ -13,56 +14,63 @@ class Player:
 
     def learn(self, my_move, their_move):
         """ Records the moves made by the player and their opponent."""
-        self.moves_result.append((my_move, their_move))
+        pass
 
 
 class AllRockPlayer(Player):
     """A player class that inherits from the Player
      class and plays always rock move"""
     def move(self):
-        return self.moves
+        return self.moves[0]
+
 
 class RandomPlayer(Player):
     """A player class that inherits from the
     Player class and plays a random move"""
     def move(self):
-        index = random.randint(0, 2)
+        index = random.randint(0, len(self.moves)-1)
         return self.moves[index]
 
 
 class HumanPlayer(Player):
-    """A player class that inherits from the Player class and allows the user to input their move."""
+    """A player class that inherits from the Player class
+    and allows the user to input their move."""
     def move(self):
         """Asks a user for input and returns when it right"""
         while True:
-            user_input = input("rock, paper, scissors? > ").lower()
-            if user_input in self.moves:
-                return user_input
+            user_input_move = input("rock, paper or scissors? > ").lower()
+            if user_input_move in self.moves:
+                return user_input_move
+            print(f"The move {user_input_move} is invalid. Try again!")
 
 
 class ReflectPlayer(Player):
-    """A player class that inherits from the Player class and plays the opponent's last move."""
+    """A player class that inherits from the Player class
+    and plays the opponent's last move."""
     def move(self):
         """Plays a move of an opponent from a last round"""
         if self.moves_result:
             return self.moves_result[-1][1]
         else:
-            index = random.randint(0, 2)
+            index = random.randint(0, len(self.moves)-1)
             return self.moves[index]
+
+    def learn(self, my_move, their_move):
+        """ Records the moves made by the player and their opponent."""
+        self.moves_result.append((my_move, their_move))
 
 
 class CyclePlayer(Player):
     """A player class that inherits from the Player class
-     and plays a move different from the opponent's last move."""
+     and cycles through the different moves in order"""
     def move(self):
         """Plays a move that is different from a last round opponent"""
         if self.moves_result:
-            opponent_last_move = self.moves_result[-1][1]
-            moves_to_make = [move for move in self.moves if move != opponent_last_move]
-            index = random.randint(0, 1)
-            return moves_to_make[index]
+            my_last_move: str = self.moves_result[-1][0]
+            my_last_move_index = self.moves.index(my_last_move)
+            my_next_move_index = (my_last_move_index + 1) % len(self.moves)
+
+            return self.moves[my_next_move_index]
         else:
-            index = random.randint(0, 2)
+            index = random.randint(0, len(self.moves)-1)
             return self.moves[index]
-
-
--- rps.py
+++ rps.py
@@ -5,7 +5,7 @@ import players
 
 def main():
     """Entry point of the program."""
-    game = Game(players.HumanPlayer(), players.ReflectPlayer())
+    game = Game(players.HumanPlayer(), players.CyclePlayer())
     game.play_game()
 
 
@@ -35,40 +35,46 @@ class Game:
         # index represent round. None is Draw, 1,2 represent players who won
         self.round_score = []
 
-    def play_round(self):
-        """Plays a single round of the game."""
-
+    def play_round(self) -> str:
+        """Plays a single round of the game.
+        returns string results of a round"""
         move1 = self.p1.move()
         move2 = self.p2.move()
         winner = self.who_won(move1, move2)
+
         output_text = f"Player 1: {move1}  Player 2: {move2} ----> "
         if winner:
             output_text += f"Player {winner} won"
         else:
             output_text += "Draw!"
-        print(output_text)
 
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
 
+        return output_text
+
     def play_game(self):
         """Plays the game with multiple rounds"""
         print("Game start!")
-        for round in range(3):
-            print(f"Round {round}:")
-            self.play_round()
+        for game_round in range(3):
+            print(f"Round {game_round}:")
+            text_result_of_round = self.play_round()
+            print(text_result_of_round)
 
         # calculate and print the winner
         out_text = "GAME RESULT: "
-        winner = self.calculate_winner()
+        winner, result = self.calculate_winner()
         if winner:
-            out_text += f"Player {winner} WON!!!"
+            out_text += f"Player {winner} WON!!! "
         else:
             out_text += "DRAW!!!"
+        out_text += f" Player1 score:{result[0]}, Player2 score: {result[1]}"
         print(out_text)
 
-    def calculate_winner(self):
-        """calculates and return the winner of the game. 1,2 if player 1,2 won and None for draw"""
+    def calculate_winner(self) -> tuple:
+        """calculates and return the winner of the game.
+        1,2 if player 1,2 won and None for draw
+        returns tuple (winner,(score_first_player,score_second_player)"""
         player_one = 0
         player_two = 0
         for result in self.round_score:
@@ -76,17 +82,18 @@ class Game:
                 player_one += 1
             elif result:
                 player_two += 1
-
         if player_one > player_two:
-            return 1
+            winner = 1
         elif player_two > player_one:
-            return 2
+            winner = 2
         else:
-            return None
+            winner = None
 
+        return winner, (player_one, player_two)
 
     def who_won(self, move1: str, move2: str):
-        """returns None for Draw, 1 or 2 if player 1 or 2 won, also save score to """
+        """returns None for Draw, 1 or 2 if player 1 or 2 won,
+        also save score to """
         # adding result of a round to self.round_score
         if beats(move1, move2):
             self.round_score.append(1)
@@ -100,4 +107,3 @@ class Game:
 
 if __name__ == '__main__':
     main()
-
